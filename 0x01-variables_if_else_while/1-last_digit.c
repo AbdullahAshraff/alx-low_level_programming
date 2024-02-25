db00d3a0388a1fc8d4dee8be2ae9c30fdda11357
Abdullah ubuntu <abdullah@ubuntu.com>
2024-02-25 22:24:14 +0200

    1111

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -14,7 +14,7 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 d = n % 10;
 
-printf("Last digit of  %d is %d ", n, d);
+printf("Last digit of %d is %d ", n, d);
 
 if (d > 5)
 printf("and is greater than 5\n");
