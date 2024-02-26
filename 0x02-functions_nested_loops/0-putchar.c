360e4ae273bcbf1d7bb5b4b9854f7dae981e3862
Abdullah ubuntu <abdullah@ubuntu.com>
2024-02-27 00:33:26 +0200

    0000

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,11 +6,19 @@
 int main(void)
 {
 	int i;
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+/*
 	char asdf[] = "_putchar";
-
 	for (i = 0 ; i < (int) sizeof(asdf); i++)
 	{
 		_putchar(asdf[i]);
-	}
+	}*/
 	return (0);
 }
