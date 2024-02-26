c8f282484cb805a2f4b0a8163e702de2c62f7de3
Abdullah ubuntu <abdullah@ubuntu.com>
2024-02-27 00:26:58 +0200

    main _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,16 @@
-#include <stdio.h>
+#include "main.h"
 /**
  * main - ths tis
  * Return: no
 */
 int main(void)
 {
-	printf("_putchar\n");
+	int i;
+	char asdf[] = "_putchar";
+
+	for (i = 0 ; i < (int) sizeof(asdf); i++)
+	{
+		_putchar(asdf[i]);
+	}
 	return (0);
 }
