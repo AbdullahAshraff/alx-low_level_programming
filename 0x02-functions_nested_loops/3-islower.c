04f7ef12b93e2aaa304c81dc34f98c76ea21136d
Abdullah ubuntu <abdullah@ubuntu.com>
2024-02-28 12:20:56 +0200

    333

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - checks if char is lowercase
+ * @c: is character
+ * Return: 1 if c is lowercase,0 otherwise
+ */
+int _islower(int c)
+{
+	if ('a' <= c <= 'z')
+		return (1);
+	return (0);
+}
