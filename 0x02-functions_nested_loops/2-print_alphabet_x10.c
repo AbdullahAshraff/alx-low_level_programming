304c48178214ffab01e0f7597ba55b431ec8f952
Abdullah ubuntu <abdullah@ubuntu.com>
2024-02-28 12:15:25 +0200

    22

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,5 +1,8 @@
 #include "main.h"
 
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line.
+ */
 void print_alphabet(void)
 {
 	char i;
@@ -9,10 +12,13 @@ void print_alphabet(void)
 	_putchar('\n');
 }
 
+/**
+ * print_alphabet_x10 - prints the alphabet 10 times
+ */
 void print_alphabet_x10(void)
 {
 	int i;
 
-	for(i=1;i<=10;i++)
+	for(i = 1 ; i <= 10 ; i++)
 		print_alphabet();
 }
